cae5523670b204716714d4db1e6fe73b029e8d29
allan <epicallan.al@gmail.com>
2018-09-27 11:43:06 +0300

    refactor and clean up

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,37 +1,37 @@
-import { Application } from 'probot'
+import "jest";
+import { Application } from "probot";
 // Requiring our app implementation
-import   myProbotApp  from '..'
+import  myProbotApp  from "../src/app/main";
+import repoCreatedPayload from "./fixtures/repos.create.json";
 
-// const repoCreatedPayload = require('./fixtures/repos.created.json')
-
-import { } from 'jest'
-
-describe('My Probot app', () => {
-  let app, github
+describe("My Probot app", () => {
+  let app;
+  let github;
 
   beforeEach(() => {
-    app = new Application()
+    app = new Application();
     // Initialize the app based on the code from index.js
-    app.load(myProbotApp)
+    app.load(myProbotApp);
     // This is an easy way to mock out the GitHub API
     github = {
       issues: {
         createComment: jest.fn().mockReturnValue(Promise.resolve({}))
       }
-    }
+    };
     // Passes the mocked out GitHub API into out app instance
-    app.auth = () => Promise.resolve(github)
-  })
+    app.auth = () => Promise.resolve(github);
+  });
 
 
-  // test('creates an issue when code is pushed to a repository', async () => {
-  //   // Simulates delivery of an issues.opened webhook
-  //   await app.receive({
-  //     name: 'push',
-  //     payload: repoCreatedPayload
-  //   })
+  test.skip("creates an issue when code is pushed to a repository", async () => {
+    // Simulates delivery of an issues.opened webHook
+    await app.receive({
+      name: "push",
+      payload: repoCreatedPayload
+    });
 
-  //   // This test passes if the code in your index.js file calls `context.github.issues.createComment`
-  //   expect(github.issues.createComment).toHaveBeenCalled()
-  // })
-})
+    // This test passes if the code in your main.ts file calls `context.github.issues.createComment`
+    expect(github.issues.createComment).toHaveBeenCalled();
+  });
+});
+
