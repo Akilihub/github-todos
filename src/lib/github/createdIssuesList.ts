417dcf40ec05e61826700ab514798138f936eb8c
lubegasimon <lubegasimonwalker@gmail.com>
2018-10-03 13:35:15 +0300

    minor modifications

--- src/lib/github/createdIssuesList.ts
+++ src/lib/github/createdIssuesList.ts
@@ -2,19 +2,24 @@
  * extracts a list of open issues created by the app
  */
 
-export interface openIssues {
+export interface OpenIssues {
     title: string; // issue title
     id: string; // issue id
-};
+}
 
-export default async function autoCreatedIssues (context: any): Promise<openIssues> {
+export default async function appCreatedIssues (context: any): Promise<OpenIssues[]> {
     const octokit = context.github;
 
     const opts = {
-      filter: 'subscribed', 
-      state: 'open'
-    }
+      filter: "subscribed", 
+      state: "open"
+    };
 
     const result = await octokit.issues.getAll(opts);
-    return result.issues.map(issue => { [ {title: issue.title}, {id: issue.number} ] });
+    return result.issues.map(issue => { 
+        return {
+            title: issue.title,
+            id: issue.number
+        } 
+    });
 }
